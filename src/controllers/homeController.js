9bfaebd5c0a16cdfa2d1969e857f8743a4647ebf
TranCuong <dev09.katec@gmail.com>
2025-01-02 17:15:52 +0700

    third commit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,4 @@
+const connection = require("../config/database");
 const getHompage = (reg, res) => {
   return res.render("home.ejs");
 };
@@ -9,9 +10,24 @@ const getAbc = (reg, res) => {
 const getCuongdeptrai = (reg, res) => {
   res.render("sample.ejs");
 };
+const postCreateUser = (req, res) => {
+  let { email, name, city } = req.body;
 
+  console.log(">>> email = ", email, " name = ", name, " city = ", city);
+
+  connection.query(
+    `  INSERT INTO 
+    Users (email, name, city) 
+    VALUES (?, ?, ?) `,
+    [email, name, city],
+    function (err, results) {
+      res.send("Them nguoi dung thanh cong");
+    }
+  );
+};
 module.exports = {
   getHompage,
   getAbc,
   getCuongdeptrai,
+  postCreateUser,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const {getHompage,getAbc,getCuongdeptrai} = require('../controllers/homeController');
+const express = require("express");
+const {
+  getHompage,
+  getAbc,
+  getCuongdeptrai,
+  postCreateUser,
+} = require("../controllers/homeController");
 const router = express.Router();
 
-router.get('/',getHompage);
-router.get('checkabc',getAbc);
-router.get('/cuongdeptrai',getCuongdeptrai);
+router.get("/", getHompage);
+router.get("checkabc", getAbc);
+router.get("/cuongdeptrai", getCuongdeptrai);
+
+router.post("/create-user", postCreateUser);
 
 module.exports = router;
